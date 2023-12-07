36f86ac40abac2c72b9eac0fbcfc23856361830a
tushar885 <tushar8104@gmail.com>
2023-12-07 11:06:45 +0530

    worked on matrimony and filterSection

--- src/pages/public/contact/FormContact.js
+++ src/pages/public/contact/FormContact.js
@@ -167,7 +167,7 @@ const FormContact = () => {
                     {message}
                   </p>
                   <p
-                    className="text-[#EF4D48] border p-1 rounded-md border-gray-900
+                    className="text-[#EF4D48] 
                    text-xl font-bold font-founder hover:cursor-pointer whitespace-nowrap"
                     onClick={() => {
                       setMessage(null);
@@ -175,10 +175,13 @@ const FormContact = () => {
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      height="1.3rem"
-                      viewBox="0 0 384 512"
+                      height="2rem"
+                      viewBox="0 0 512 512"
+                      // fill="#EF4D48"
+                      className="fade-in fill-green-700"
                     >
-                      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                      {/*!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.*/}
+                      <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
                     </svg>
                   </p>
                 </div>

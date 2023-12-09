a0c5af03682daf9606e323b6632001e0c4c41b19
Tanish anand <imta819@gmail.com>
2023-12-09 12:26:56 +0530

    tested & done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 
 const Footer = () => {
-  const [visitorCount, setVisitorCount] = useState(0);
+  const [visitorCount, setVisitorCount] = useState(5567);
 
   // useEffect(() => {
   //   const fetchData = async () => {
